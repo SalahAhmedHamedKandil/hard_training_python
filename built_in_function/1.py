4982c0e9de0e2828fb82c395a20a8511469765d6
SalahAhmedHamedKandil <sa0kandil@gmail.com>
2025-11-26 00:24:03 +0200

    update2

--- built_in_function/1.py
+++ built_in_function/1.py
@@ -6,12 +6,31 @@
 # id()
 # -----------------
 
-x = [1,2,3,4,5]
-if all(x):
-  print("all elements are true")
-else:
-  print("there is at least  one element is false")
+# x = [1,2,3,4,5,0]
+# if all(x):
+#   print("all elements are true")
+# else:
+#   print("there is at least  one element is false")
   
+# y = [1,2,3,4,5,[]]
+# if any(y):
+#   print("there is at least  one element is true")
+# else:
+#   print("all elements are false")
+
+
+# -------------
+
+# print(bin(100))
+
+# --------------------
+
+a=1  
+b=2
+print(id(a))
+print(id(b))
+
+
 
 
 
