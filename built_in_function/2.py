d28744a426a7de3405f60803845fe2a50415f2d3
SalahAhmedHamedKandil <sa0kandil@gmail.com>
2025-11-26 12:10:31 +0200

    update

---
+++ built_in_function/2.py
@@ -0,0 +1,39 @@
+# built in function
+# -----------------
+# sum()
+# round()
+# range()
+# print()
+# -----------------
+
+# sum(iterable,start)
+
+# a=[10,10,10,20,30]
+# print(sum(a))
+# print(sum(a,40))
+
+# -------------------
+
+# round(number,numofdigits)
+
+# print(round(150.501))
+
+# print(round(150.111111,2))
+
+# ---------------------------
+
+# # range (start,end,step) #make a list 
+# print(list(range(10))) #10 is not incload
+# print(list(range(0,10,2)))
+# print(list(range(1,10,2)))
+
+# -----------------------------
+#  print()
+print("hello salah ahmed ")
+print("hello" , "salah" , "ahmed ")
+print("hello" , "salah" , "ahmed ",sep=(" $ "))
+print("hello" , "salah" , "ahmed ",sep=(" # "))
+
+print("hello",end=(" "))
+print("salah")
+
