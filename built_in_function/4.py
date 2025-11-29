cfe9d80285a198a9b0588298eafd27d27daef7e2
SalahAhmedHamedKandil <sa0kandil@gmail.com>
2025-11-29 23:58:32 +0200

    filter

--- built_in_function/4.py
+++ built_in_function/4.py
@@ -4,16 +4,24 @@
 # 2 - the function van be pre-defined function or lambda function
 # ------------------------------------
 
-def formaeText(text):
-  return f"- {text} -"
+# def formaeText(text):
+#   return f"- {text} -"
 
-myText=["salah","ahmed","hamed"]
-myFormatedData=map(formaeText,myText)
-print(myFormatedData)
-for name in myFormatedData:
-  print(name)
-print("-_"*10)
+# myText=["salah","ahmed","hamed"]
+# myFormatedData=map(formaeText,myText)
+# print(myFormatedData)
+# for name in myFormatedData:
+#   print(name)
+# print("-_"*10)
+# # # output
+# # - salah -
+# # - ahmed -
+# # - hamed -
 # -------------------------
 myText=["salah","ahmed","hamed"]
 for name in map(lambda text:  f"- {text.strip().capitalize()} -",myText):
   print(name)
+# output
+# - Salah -
+# - Ahmed -
+# - Hamed -
--- built_in_function/5_filter.py
+++ built_in_function/5_filter.py
@@ -6,15 +6,27 @@
 # 4-filter out all elements for which the function return true
 # 5-the fuction need to return boolean value
 # --------------------
-def formaeText(text):
-  return f"- {text} -"
-
-myText=["salah","ahmed","hamed"]
-myFormatedData=filter(formaeText,myText)
-print(myFormatedData)
-for name in myFormatedData:
+mynames=["salah","ahmed","hamed","amer","samy"]
+def formate_name(name):
+  return name.startswith("a") 
+my_formate_name=filter(formate_name,mynames)
+print(my_formate_name)
+for name in my_formate_name:
   print(name)
 print("-_"*10)
 
+# --- output ----
+    # ahmed
+    # amer
 
+
+
+for name in filter(lambda name : name.startswith("a") or ("s")  ,mynames):
+  print (name.capitalize())  #we used or
+# # # output
+#   # Salah
+#   # Ahmed
+#   # Hamed
+#   # Amer
+#   # Samy
 
