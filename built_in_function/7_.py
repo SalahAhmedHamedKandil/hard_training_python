460e43900d2cdd9374caa47a8c64a4b5f2c90a50
SalahAhmedHamedKandil <sa0kandil@gmail.com>
2025-12-03 19:57:19 +0200

    clinic_project_finshed

--- built_in_function/7_.py
+++ built_in_function/7_.py
@@ -5,5 +5,16 @@
 # --------------------
 
 # enumerate(iterable,start=0)
-my_skills=["soeakin","writeing",""]
+my_skills=["html","Css","Js","PHP"]
+mySkillWithCounter=enumerate(my_skills,1)
+# print(type(mySkillWithCounter)) => 'enumerate'
+# print(mySkillWithCounter) => <enumerate object at 0x000002572D911F30>
+# print(list(mySkillWithCounter)) =>[(1, 'html'), (2, 'Css'), (3, 'Js'), (4, 'PHP')]
+for skill in mySkillWithCounter:
+  print(skill)
+# output
+# (1, 'html')
+# (2, 'Css')
+# (3, 'Js')
+# (4, 'PHP')
 
